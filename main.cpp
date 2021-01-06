4460d47d5377924877eba21b31d97c8d7c4c5c24
Chilarai <chilly5476@gmail.com>
2021-01-06 23:16:19 +0530

    link path changed

--- main.cpp
+++ main.cpp
@@ -4,33 +4,33 @@
  * =====================================
  */
 
-
 #include <iostream>
-#include "duckdb.hpp"
+#include "src/include/duckdb.hpp"
 
 using namespace duckdb;
 
-int main(int argc, char* argv[]) {	
+int main(int argc, char *argv[])
+{
 
 	DuckDB db(nullptr);
 	Connection con(db);
 	auto result = con.Query("SELECT 42");
-	
+
 	// Basic create table and insert
 	con.Query("CREATE TABLE people(id INTEGER, name VARCHAR)");
-    con.Query("CREATE TABLE test");
-    con.Query("INSERT INTO people VALUES (0,'Mark'), (1, 'Hannes')");	
+	con.Query("CREATE TABLE test");
+	con.Query("INSERT INTO people VALUES (0,'Mark'), (1, 'Hannes')");
 
 	// Insert using appender
 	duckdb::Appender appender(con, "people");
-    appender.AppendRow(2, "Chilarai");
+	appender.AppendRow(2, "Chilarai");
 	appender.AppendRow(3, "Test");
-    appender.Close();
+	appender.Close();
 
 	// Update data
 	auto prepared = con.Prepare("UPDATE people SET name = $1 WHERE id = $2");
-    auto prep = prepared->Execute("DuckDb", 3);
-	
+	auto prep = prepared->Execute("DuckDb", 3);
+
 	// Delete data
 	auto resultDelete = con.Query("DELETE FROM people WHERE id = 3");
 
